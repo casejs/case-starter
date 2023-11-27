0f2794f428676cded97ab70842c29284c14fad9d
Bruno <bruno@buddyweb.fr>
2023-11-27 15:29:13 +0100

    feat: link to doc in placeholder

--- entities/placeholder.ts
+++ entities/placeholder.ts
@@ -1,2 +1,2 @@
-// TSCONFIG placeholder: You can remove this file if you have entities in this folder.
+// tsconfig placeholder: Remove this file once you have entities in this folder: https://docs.case.app/entities
 export {};
